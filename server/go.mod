02506410482439db91bda8e3b712bdd63e5fe327
tonyzzp <tonyzzp@qq.com>
2024-05-10 14:01:32 +0800

    server logger

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,9 @@ go 1.22.2
 require (
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/glebarez/sqlite v1.11.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+	gorm.io/gorm v1.25.10
 )
 
 require (
@@ -16,7 +19,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/glebarez/go-sqlite v1.21.2 // indirect
-	github.com/glebarez/sqlite v1.11.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
@@ -26,9 +28,9 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
@@ -42,8 +44,6 @@ require (
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/sqlite v1.5.5 // indirect
-	gorm.io/gorm v1.25.10 // indirect
 	modernc.org/libc v1.22.5 // indirect
 	modernc.org/mathutil v1.5.0 // indirect
 	modernc.org/memory v1.5.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,23 +1,62 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"slices"
 	"spc-server/api"
 	"spc-server/db"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 const SIGN_KEY = "15998569"
+
+var isDocker = false
+var loggerWriter io.Writer
+
+func init() {
+	envs := os.Environ()
+	log.Println(envs)
+	isDocker = slices.Contains(envs, "ISDOCKER=true")
+}
+
+func initLogger() {
+	_l := &lumberjack.Logger{
+		MaxSize:    10,
+		MaxAge:     60,
+		MaxBackups: 30,
+		LocalTime:  true,
+		Compress:   false,
+	}
+	if isDocker {
+		_l.Filename = "/data/logs/spc.log"
+	} else {
+		_l.Filename = "spc.log"
+	}
+	loggerWriter = io.MultiWriter(_l, os.Stdout)
+	log.SetOutput(loggerWriter)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func initDB() {
+	dbPath := "spc-db.sqlite"
+	if isDocker {
+		dbPath = "/data/spc-db.sqlite"
+	}
+	log.Println("dbpath", dbPath)
+	db.Init(dbPath)
+}
 
 func findStaticDir() string {
 	exec, _ := os.Executable()
 	log.Println("exec", exec)
 
-	dir := filepath.Join(exec, "static/index.html")
+	dir := filepath.Join(exec, "../static/index.html")
 	if fi, _ := os.Stat(dir); fi != nil {
 		dir = filepath.Dir(dir)
 		dir, _ = filepath.Abs(dir)
@@ -48,10 +87,12 @@ func findStaticDir() string {
 }
 
 func main() {
-	db.Init("spc-db.sqlite")
+	initLogger()
+	initDB()
+
 	g := gin.New()
 	g.Use(cors.Default())
-	g.Use(gin.Logger())
+	g.Use(gin.LoggerWithWriter(loggerWriter))
 	staticDir := findStaticDir()
 	log.Println("staticDir", staticDir)
 	if staticDir != "" {
@@ -62,5 +103,5 @@ func main() {
 	g.POST("/api/changePassword", api.WithVerifySign(SIGN_KEY, api.ChangePassword))
 	g.POST("/api/save", api.WithVerifySign(SIGN_KEY, api.Save))
 	g.POST("/api/load", api.WithVerifySign(SIGN_KEY, api.Load))
-	g.Run(":8080")
+	g.Run(":http")
 }
